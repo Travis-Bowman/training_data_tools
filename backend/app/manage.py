bb3e44de921383da716957374bd747a789014099
Travis.Bowman <tbowman@ltu.edu>
2024-10-16 11:52:32 +0300

    Adding CLI

---
+++ backend/app/manage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import argparse
+
+#Create a parser object, prog= name of program, description=gives useser help
+parser = argparse.ArgumentParser(prog="Training data Tools",description="Input the file path to the folder you want to organize.")
+
+#adds argument-- if you need more then one args all multiple lines
+parser.add_argument('file_path',type=str,help="Write the file path")
+
+#parsers the arguments
+args = parser.parse_args()
+
+#once the args are parsered args.(the argument will bring the input into the script)
+file_path = args.file_path
+
